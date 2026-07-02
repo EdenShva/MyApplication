1154426be1ac616824e15499fe27a512330d5b34
EdenShva <edens1003@gmail.com>
2026-07-02 17:10:26 +0300

    Add navigation component and main fragments

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
@@ -16,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.navHostFragment);
+
+        if (navHostFragment != null) {
+            NavigationUI.setupWithNavController(
+                    binding.bottomNavigationView,
+                    navHostFragment.getNavController()
+            );
+        }
     }
 }
